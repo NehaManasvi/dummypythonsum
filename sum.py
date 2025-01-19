24d21ecc2cbf4dfa90fd392c7128517ea755fd3a
NehaManasvi <131847502+NehaManasvi@users.noreply.github.com>
2025-01-19 07:38:06 +0530

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,8 @@
+def sum_two_numbers(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    result = sum_two_numbers(num1, num2)
+    print(f"The sum of {num1} and {num2} is {result}")
